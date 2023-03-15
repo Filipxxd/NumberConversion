acfc68811c0f0a835400b92d6e07ffc6be0d3002
Filip Chromečka <chromda08@gmail.com>
2023-03-15 20:09:45 +0100

    Rewritten & Simplified.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using Spectre.Console;
 using System.Text.RegularExpressions;
+using static NumberConversion.TranslationManager;
 
 namespace NumberConversion
 {
@@ -9,7 +10,7 @@ namespace NumberConversion
         {
             if (!AnsiConsole.Profile.Capabilities.Interactive)
             {
-                AnsiConsole.Write(new Markup($"[red]Environment does not support interaction[/]" + Environment.NewLine));
+                Console.WriteLine("Environment does not support interaction");
                 return;
             }
 
@@ -18,41 +19,40 @@ namespace NumberConversion
                 Console.BackgroundColor = ConsoleColor.Black;
             }
 
-            AnsiConsole.Write(
-                new FigletText("Number Conversion")
-                    .Centered()
-                    .Color(Color.Red));
+            AnsiConsole.Write(new FigletText("Number Conversion").Centered().Color(Color.Red));
 
-            WriteDivider("Number");
-            string number = AskInputNumber();
+            bool askAgain = true;
+            while (askAgain)
+            {
+                WriteDivider("Number");
+                string number = AskInputNumber();
 
-            WriteDivider("Input Numerical System");
-            string inputSystem = AskInputSystem();
+                WriteDivider("Input Numerical System");
+                string inputSystem = AskInputSystem();
 
-            WriteDivider("Output Numerical System(s)");
-            List<string> outputSystems = AskOutputSystems();
+                long decimalNumber;
+                try
+                {
+                    NumericalSystem system = IdentifySystem(inputSystem);
+                    decimalNumber = TryConversionFrom(system, number);
 
-            WriteDivider("Result");
-            Dictionary<string, string> outputValues = TranslationManager.Translate(number, inputSystem, outputSystems);
-            DisplayTranslationsToConsole(outputValues);
+                    WriteDivider("Output Numerical System(s)");
+                    List<string> outputSystems = AskOutputSystems(inputSystem);
+
+                    WriteDivider("Result");
+                    WriteTranslations(outputSystems, decimalNumber);
+
+                    askAgain = false;
+                }
+                catch (Exception)
+                {
+                    AnsiConsole.MarkupLine($"[red]There was an error while trying to convert number into '{inputSystem}'.[/]");
+                    askAgain = AnsiConsole.Confirm("Do you want to start over?");
+                }
+            }
 
             WriteDivider("Press Any To Close");
             Console.ReadLine();
-
-        }
-
-        /// <summary>
-        /// Displays the results of a translation process to the console.
-        /// </summary>
-        /// <param name="translations">A dictionary containing the translations.</param>
-        private static void DisplayTranslationsToConsole(Dictionary<string, string> translations)
-        {
-            var table = new Table().AddColumns("[grey]Numerical system[/]", "[grey]Translation[/]").Centered();
-            foreach (var item in translations)
-            {
-                table.AddRow(item.Key, item.Value).Centered();
-            }
-            AnsiConsole.Write(table);
         }
 
         /// <summary>
@@ -64,8 +64,9 @@ namespace NumberConversion
             AnsiConsole.Write(new Rule(Environment.NewLine + $"[yellow]{text}[/]").RuleStyle("grey").LeftJustified());
         }
 
-        [GeneratedRegex("^[a-zA-Z0-9-.,]+$")]
-        private static partial Regex MyRegex();
+        [GeneratedRegex("^[a-zA-Z0-9]+$")]
+        private static partial Regex InputRegex();
+        private static readonly int MaxCharactersCount = 50;
 
         /// <summary>
         /// Asks the user to input a number to translate.
@@ -73,13 +74,17 @@ namespace NumberConversion
         /// <returns>The user's input.</returns>
         private static string AskInputNumber()
         {
-            Regex regex = MyRegex();
+            Regex regex = InputRegex();
 
             return AnsiConsole.Prompt(new TextPrompt<string>("Please type in a [green]number[/] to translate: ")
                 .PromptStyle("green")
-                .Validate(userInput => regex.IsMatch(userInput) && userInput.Length < 50 ?
-                    ValidationResult.Success() :
-                    ValidationResult.Error("[red]Please type in a valid number no longer than 50 chars.[/]")));
+                .Validate(userInput =>
+                    regex.IsMatch(userInput) && userInput.Length < MaxCharactersCount ?
+                        ValidationResult.Success() :
+                        ValidationResult.Error(
+                            "[red]Please type in a valid number that has no more than 50 chars.[/]" +
+                            Environment.NewLine +
+                            $"[red]Bear in mind that only positive whole numbers are accepted![/]")));
         }
 
         /// <summary>
@@ -88,35 +93,48 @@ namespace NumberConversion
         /// <returns>The user's selection.</returns>
         private static string AskInputSystem()
         {
-            var selection = AnsiConsole.Prompt(
+            return AnsiConsole.Prompt(
                 new SelectionPrompt<string>()
                     .Title("Which [green]numerical system[/] is your number from?")
                     .MoreChoicesText("[grey](Move up and down via arrow keys)[/]")
-                    .AddChoices(TranslationManager.NumericalSystems));
-
-            // Remove the user selected input system from the list of all systems.
-            TranslationManager.NumericalSystems.Remove(selection);
-
-            return selection;
+                    .AddChoices(Enum.GetNames(typeof(NumericalSystem))));
         }
 
         /// <summary>
         /// Asks the user to select the output numerical system(s).
         /// </summary>
         /// <returns>The user's selection.</returns>
-        private static List<string> AskOutputSystems()
+        private static List<string> AskOutputSystems(string selectedSystem)
         {
+            List<string> availableSystems = new(Enum.GetNames(typeof(NumericalSystem)));
+            availableSystems.Remove(selectedSystem);
+
             var selection = AnsiConsole.Prompt(
                 new MultiSelectionPrompt<string>()
-                    .PageSize(10)
                     .Title("Which [green]numerical system(s)[/] do you want to translate into?")
                     .MoreChoicesText("[grey](Move up and down via arrow keys)[/]")
                     .InstructionsText("[grey](Press [blue]<spacebar>[/] to select a numerical system, [green]<enter>[/] to accept)[/]")
-                    .AddChoices(TranslationManager.NumericalSystems));
+                    .AddChoices(availableSystems));
 
-            // To make output in same order as selection - ascending.
             selection.Reverse();
             return selection;
+        }
+
+        /// <summary>
+        /// Displays the results of a translation process to the console.
+        /// </summary>
+        /// <param name="translations">A dictionary containing the translations.</param>
+        private static void WriteTranslations(List<string> outputSystems, long decimalNumber)
+        {
+            var table = new Table().AddColumns("[grey]Numerical system[/]", "[grey]Translation[/]").Centered();
+            table.Columns[1].Alignment(Justify.Right);
+
+            foreach (string outputSystem in outputSystems)
+            {
+                NumericalSystem outputNumericalSystem = IdentifySystem(outputSystem);
+                table.AddRow(outputSystem, Translate(decimalNumber, outputNumericalSystem));
+            }
+            AnsiConsole.Write(table);
         }
     }
 }
